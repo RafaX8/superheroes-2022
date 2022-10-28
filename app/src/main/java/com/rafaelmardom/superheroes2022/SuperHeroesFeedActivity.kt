d3c77f8e389b5628e96491cf18609cbffc6af77e
RafaX8 <rafita8md@gmail.com>
2022-10-28 15:33:04 +0200

    Proyecto Creado

---
+++ app/src/main/java/com/rafaelmardom/superheroes2022/SuperHeroesFeedActivity.kt
@@ -0,0 +1,11 @@
+package com.rafaelmardom.superheroes2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuperHeroesFeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_superhero_feed)
+    }
+}
